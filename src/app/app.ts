ee321b07ff21d9a252d83451f2670a4d0918ef6b
vaniaferreresteban <vaniaferreresteban@gmail.com>
2025-06-05 13:33:11 +0200

    feat: create component home in app

---
+++ src/app/app.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+import {Home} from './components/home/home'
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet,Home],
+  templateUrl: './app.html',
+  styleUrl: './app.scss'
+})
+export class App {
+  protected title = 'S5';
+}
